a06219fb936e64a53e0d44334b2eb351e9c15979
afolabiopakunle <opakunleafolabi@gmail.com>
2022-11-28 22:11:39 +0100

    static animation works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
+import { state, trigger, style } from '@angular/animations';
 import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  animations: [
+    trigger('divState', [
+      state('start', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0)'
+      })),
+      state('end', style({
+        backgroundColor: 'green',
+        transform: 'translateX(140px)'
+      }))
+    ])
+  ]
 })
 export class AppComponent  {
 
+  state = 'start'
   list = ["Milk", "Chocolate", "Sugar"]
 
-  onAnimate() {}
+  onAnimate() {
+    this.state === 'start' ? this.state = 'end' : this.state = 'start'
+  }
 
   onShrink() {}
 
